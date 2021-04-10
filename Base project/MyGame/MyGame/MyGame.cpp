c38bdb3f1e847db84df7acd75cdb30a627c00425
SundarK407 <70714827+SundarK407@users.noreply.github.com>
2021-04-10 13:02:21 -0700

    Part 1

--- Base project/MyGame/MyGame/MyGame.cpp
+++ Base project/MyGame/MyGame/MyGame.cpp
@@ -6,6 +6,37 @@ const int WINDOW_HEIGHT = 600;
 
 const std::string WINDOW_TITLE = "Desolation";
 
+#pragma once
+
+#include <set>
+#include <string>
+#include <memory>
+#include <SFML/Graphics.hpp>
+
+class GameObject
+{
+	public:
+		void assignTag(std::string tag);
+		bool hasTag(std::string tag);
+
+		bool isDead();
+		void makeDead();
+		virtual void update(sf::Time& elasped) {}
+		virtual void draw(){}
+		bool isCollisionCheckEnabled();
+		void setCollisionCheckEnabled(bool isCollisionCheckEnabled);
+		virtual sf::FloatRect getCollisionRect() { return sf::FloatRect(); }
+		virtual void handleEvent (sf::Event& event){}
+
+	private:
+		std::set<std::string> tags_;
+
+		bool isDead_ = false;
+		bool isCollisionCheckEnabled_ = false;
+
+
+};
+typedef std::shared_ptr<GameObject> GameObjectPtr;
 int main()
 {
 	// Seed the random number generator.
